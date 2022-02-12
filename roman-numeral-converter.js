24007baae33390d4c5ea5441ccd8afd5afdafe45
Ahmed Helal Ahmed <ahmed.helal.online@gmail.com>
2022-02-12 22:15:48 +0200

    DRY and use OOP

--- roman-numeral-converter.js
+++ roman-numeral-converter.js
@@ -1,98 +1,44 @@
 // https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/javascript-algorithms-and-data-structures-projects/roman-numeral-converter
-function convertToRoman(num) {
-    let result = "";
-// M: 1000
-// D: 500
-// C:100
-// L:50
-
-    while (num >= 1000) {
-        result += "M";
-        num -= 1000;
-    }
-    console.log(`Finished with M, leftover: ${num}`);
-
-
-    while (num >= 900) {
-        result += "CM";
-        num -= 900;
-    }
-    console.log(`Finished with CM, leftover: ${num}`);
-
-
-    while (num >= 500) {
-        result += "D";
-        num -= 500;
-    }
-    console.log(`Finished with D, leftover: ${num}`);
-
-
-    while (num >= 400) {
-        result += "CD";
-        num -= 400;
-    }
-    console.log(`Finished with CD, leftover: ${num}`);
-
-
-    while (num >= 100) {
-        result += "C";
-        num -= 100;
-    }
-    console.log(`Finished with C, leftover: ${num}`);
-
-    while (num >= 90) {
-        result += "XC";
-        num -= 90;
-    }
-    console.log(`Finished with XC, leftover: ${num}`);
-
-
-    while (num >= 50) {
-        result += "L";
-        num -= 50;
-    }
-    console.log(`Finished with L, leftover: ${num}`);
-
-    while (num >= 40) {
-        result += "XL";
-        num -= 40;
-    }
-    console.log(`Finished with XL, leftover: ${num}`);
-
-    while (num >= 10) {
-        result += "X";
-        num -= 10;
-    }
-    console.log(`Finished with X, leftover: ${num}`);
-
-
-    while (num >= 9) {
-        result += "IX";
-        num -= 9;
-    }
-    console.log(`Finished with IX, leftover: ${num}`);
-
-    while (num >= 5) {
-        result += "V";
-        num -= 5;
-    }
-    console.log(`Finished with V, leftover: ${num}`);
-
-    while (num >= 4) {
-        result += "IV";
-        num -= 4;
-    }
-    console.log(`Finished with IV, leftover: ${num}`)
-
-    while (num >= 1) {
-        result += "I";
-        num -= 1;
+// Don't repeat you self
+class NumeralConverter {
+    constructor(numberToConvert) {
+        this.value = numberToConvert;
+        this.numeral = "";
     }
 
-    return result;
+    processNumeral(symbol, symbolValue) {
+        while (this.value >= symbolValue) {
+            this.numeral += symbol;
+            this.value -= symbolValue;
+        }
+    }
 }
 
-console.log(convertToRoman(1000));
+function convertToRoman(num) {
+    const converter = new NumeralConverter(num);
+    const ROMAN_NUMBERS_MAP = {
+        'M': 1000,
+        'CM': 900,
+        'D': 500,
+        'CD': 400,
+        'C': 100,
+        'XC': 90,
+        'L': 50,
+        'XL': 40,
+        'X': 10,
+        'IX': 9,
+        'V': 5,
+        'IV': 4,
+        'I': 1,
+    };
+
+    for (let symbol in ROMAN_NUMBERS_MAP) {
+        converter.processNumeral(symbol, ROMAN_NUMBERS_MAP[symbol]);
+    }
+    return converter.numeral;
+}
+
+console.log(convertToRoman(99));
 
 // M: 1000
 // D: 500
